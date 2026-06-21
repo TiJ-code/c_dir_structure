c9e8cfebe1c7aa88d04c53203a55bc17a5281b39
TιJ <78140568+TiJ-code@users.noreply.github.com>
2026-06-21 23:44:49 +0200

    Added basic cli workflow

--- structure.c
+++ structure.c
@@ -1,6 +1,41 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-  printf("Hello World!\n");
+typedef int32_t int32;
+
+#define MAX 256
+#define MAX_MODULES 100
+#define END_KEYWORD "#END"
+
+int32 main(void) {
+  char root[MAX];
+
+  char modules[MAX_MODULES][MAX];
+
+  int32 count = 0;
+
+  printf("Root dir:\n> ");
+  scanf("%255s", root);
+
+  printf("\nModules (exit: %s):\n", END_KEYWORD);
+
+  while (count < MAX_MODULES) {
+    printf("> ");
+
+    scanf("%255s", modules[count]);
+
+    if (!strcmp(modules[count], END_KEYWORD)) {
+      break;
+    }
+
+    count++;
+  }
+
+  printf("\nCreating structure...\n");
+
+  puts("Done.");
+
   return 0;
 }
